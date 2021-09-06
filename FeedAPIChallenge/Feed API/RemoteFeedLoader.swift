559faa9d688889231e6b52b8211664cd1c265763
Angela Lin <angela.t.lin@gmail.com>
2021-09-05 21:17:37 -0400

    Guard against loading when class is already deallocated

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case .success((let data, let response)):
 				guard response.statusCode == 200 else {
--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -92,40 +92,40 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 		})
 	}
 
-//	func test_load_deliversSuccessWithItemsOn200HTTPResponseWithJSONItems() {
-//		let (sut, client) = makeSUT()
-//
-//		let item1 = makeItem(
-//			id: UUID(),
-//			imageURL: URL(string: "http://a-url.com")!)
-//
-//		let item2 = makeItem(
-//			id: UUID(),
-//			description: "a description",
-//			location: "a location",
-//			imageURL: URL(string: "http://another-url.com")!)
-//
-//		let items = [item1.model, item2.model]
-//
-//		expect(sut, toCompleteWith: .success(items), when: {
-//			let json = makeItemsJSON([item1.json, item2.json])
-//			client.complete(withStatusCode: 200, data: json)
-//		})
-//	}
-//
-//	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-//		let url = URL(string: "http://any-url.com")!
-//		let client = HTTPClientSpy()
-//		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-//
-//		var capturedResults = [RemoteFeedLoader.Result]()
-//		sut?.load { capturedResults.append($0) }
-//
-//		sut = nil
-//		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-//
-//		XCTAssertTrue(capturedResults.isEmpty)
-//	}
+	func test_load_deliversSuccessWithItemsOn200HTTPResponseWithJSONItems() {
+		let (sut, client) = makeSUT()
+
+		let item1 = makeItem(
+			id: UUID(),
+			imageURL: URL(string: "http://a-url.com")!)
+
+		let item2 = makeItem(
+			id: UUID(),
+			description: "a description",
+			location: "a location",
+			imageURL: URL(string: "http://another-url.com")!)
+
+		let items = [item1.model, item2.model]
+
+		expect(sut, toCompleteWith: .success(items), when: {
+			let json = makeItemsJSON([item1.json, item2.json])
+			client.complete(withStatusCode: 200, data: json)
+		})
+	}
+
+	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+		let url = URL(string: "http://any-url.com")!
+		let client = HTTPClientSpy()
+		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut?.load { capturedResults.append($0) }
+
+		sut = nil
+		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+		XCTAssertTrue(capturedResults.isEmpty)
+	}
 
 	// MARK: - Helpers
 
