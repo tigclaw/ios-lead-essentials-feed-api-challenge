2aed0f3fef3828c853812bc10dd7475a8b74ddee
Angela Lin <angela.t.lin@gmail.com>
2021-09-06 11:17:59 -0400

    Move the network-specific models into the mapper and make private

--- FeedAPIChallenge/Feed API/FeedImageMapper.swift
+++ FeedAPIChallenge/Feed API/FeedImageMapper.swift
@@ -9,6 +9,28 @@
 import Foundation
 
 internal class FeedImageMapper {
+	private struct NetworkFeedImage: Decodable {
+		let id: UUID
+		let description: String?
+		let location: String?
+		let url: URL
+
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+
+		var feedImage: FeedImage {
+			return FeedImage(id: id, description: description, location: location, url: url)
+		}
+	}
+
+	private struct NetworkFeedImageContainer: Decodable {
+		let items: [NetworkFeedImage]
+	}
+
 	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
 		guard response.statusCode == 200 else {
 			throw RemoteFeedLoader.Error.invalidData
